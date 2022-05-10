5f0c61f0705b583bc4d48bd601563f52a64dc11b
Davi Felipe Führ <105289033+davifuhr@users.noreply.github.com>
2022-05-10 11:45:16 -0300

    Add files via upload

---
+++ 00.py
@@ -0,0 +1,6 @@
+from turtle import position
+import pyautogui
+import time
+#descobrindo a posição do mouse na tela para clicar 
+time.sleep(10) #descansa para dar tempo de botar o mouse onde quiser
+print(pyautogui.position()) #pega e printa o valor 
